e4fab89f3e2d89a527e178ecd7529f121f699429
icontrolrastreamento-commits <icontrolrastreamento@gmail.com>
2026-01-20 17:49:47 -0300

    yt config example

---
+++ youtube_config,example.py
@@ -0,0 +1,6 @@
+# Configurações OAuth do YouTube
+# Obtenha estas credenciais no Google Cloud Console:
+# https://console.cloud.google.com/
+
+YOUTUBE_CLIENT_ID = "123123123123123abcabc.apps.googleusercontent.com"
+YOUTUBE_CLIENT_SECRET = "ABCD-mF123123123123123"
